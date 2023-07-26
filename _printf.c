0f5666fac843eb07a5d1e1319abff5afd43a5ebc
slwambui <slwambui@LTTECHMAC333.local>
2023-07-26 05:23:19 +0300

    printf for c and string - betty fix

--- _printf.c
+++ _printf.c
@@ -1,47 +1,96 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int _printf(const char *format, ...) {
-    va_list args;
-    int count = 0;
-    char c;
-    const char *s;
+void print_char(va_list arg);
+int print_string(va_list arg);
 
-    va_start(args, format);
-    while (*format) {
-        if (*format == '%') {
-            format++; // Move past '%'
-            switch (*format) {
-                case 'c':
-                    c = va_arg(args, int);
-                    putchar(c);
-                    count++;
-                    break;
-                case 's':
-                    s = va_arg(args, const char *);
-                    while (*s) {
-                        putchar(*s);
-                        s++;
-                        count++;
-                    }
-                    break;
-                case '%':
-                    putchar('%');
-                    count++;
-                    break;
-                default:
-                    putchar('%');
-                    putchar(*format);
-                    count += 2;
-                    break;
-            }
-        } else {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
+/**
+ * _printf-printf replica
+ * @format: format specifier
+ * Return: count of printed objects
+*/
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
 
-    va_end(args);
-    return count;
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					print_char(args);
+					count++;
+					break;
+				case 's':
+					count = print_string(args);
+					break;
+				case '%':
+					putchar('%');
+					count++;
+					break;
+				default:
+					putchar('%');
+					putchar(*format);
+					count += 2;
+					break;
+			}
+		}
+		else
+		{
+			putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
+
+/**
+ * print_char - Prints a char.
+ * @arg: A list of arguments pointing to
+ *       the character to be printed.
+ * Return: n/a
+ */
+void print_char(va_list arg)
+{
+	char letter;
+
+	letter = va_arg(arg, int);
+	putchar(letter);
+}
+
+/**
+ * print_string - Prints a string.
+ * @arg: A list of arguments pointing to
+ *       the string to be printed.
+ * Return: count of objs
+ */
+int print_string(va_list arg)
+{
+	char *str, *n = "(nil)";
+	int count = 0;
+
+	str = va_arg(arg, char *);
+	if (str == NULL)
+	{
+		while (*n != '\0')
+		{
+			putchar(*n);
+			n++;
+		}
+	}
+	while (*str)
+	{
+		putchar(*str);
+		str++;
+		count++;
+	}
+
+	return (count);
 }
